a50404d04f45ceedc93c574612d2d97597ad2426
Akshaykj2000 <akshaykj2000@gmail.com>
2024-02-09 11:48:58 +0530

    Password security

--- controller/blogRoter.js
+++ controller/blogRoter.js
@@ -1,13 +1,34 @@
 const express =require("express")
 const signUpModels =require("../model/signUpModel")
+const bcrypt =require("bcryptjs")
+
 
 const router = express.Router()
 
+hashPaswordGenerator =async(pass)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
 router.post("/signup", async(req,res)=>{
-    let data = req.body
-    let user =new signUpModels(data)
-    let result =await  user.save()
-    res.json({status:"success"})
+    let {data} = {"data":req.body}
+      let password =data.password
+        hashPaswordGenerator(password).then(
+            (hashedPassword)=>{
+                console.log(hashedPassword)
+                data.password=hashedPassword
+                console.log(data)
+                
+                 let user =new signUpModels(data)
+                let result =user.save()
+                
+                res.json({status:"success"})
+            }
+        )
+
+   
 })
 
 module.exports =router
+
+
